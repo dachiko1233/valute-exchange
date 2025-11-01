87a3ee26e6dd02a4b24147fd796f670dbfcc04b3
dachiko <dachimari9@gmail.com>
2025-11-01 22:20:58 +0200

    new Project

--- src/Country.jsx
+++ src/Country.jsx
@@ -3,53 +3,72 @@ import { useEffect, useState } from "react";
 export default function Country() {
   return (
     <div>
-      <GetCountryData />
+      <GetCountryData country="cyprus" />
     </div>
   );
 }
 
-function GetCountryData() {
-  const [data, setData] = useState(null);
-  const [country, setCountry] = useState("cyprus");
+function GetCountryData({ country }) {
+  const [mainCuntry, setMineCuntry] = useState(null);
+  const [neighbourCountry, setNeighbourCountry] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     async function fetchCountry() {
       try {
         const res = await fetch(`https://restcountries.com/v2/name/${country}`);
         if (!res.ok) throw new Error("Something wrong with API");
-        const json = await res.json();
+        const [data] = await res.json();
 
-        setData(json[0]);
+        setMineCuntry(data);
+        const neighbourCode = data.borders?.[0];
+        if (!neighbourCode) return;
+
+        const res2 = await fetch(
+          `https://restcountries.com/v2/alpha/${neighbourCode}`
+        );
+        if (!res2.ok) throw new Error("Neighbour not found");
+        const data2 = await res2.json();
+        setNeighbourCountry(data2);
       } catch (err) {
         console.error(err.message);
       }
     }
 
     fetchCountry();
-  }, []);
+  }, [country]);
 
-  if (!data) return <p>Loading...</p>;
+  if (error) return <p>{error}</p>;
+  if (!mainCuntry) return <p>Loading...</p>;
 
   return (
-    <div className="country">
-      <img src={data.flag} alt={data.name} className="country__img" />
+    <div className="country-flex">
+      <CountryCard data={mainCuntry} />
+      {neighbourCountry && (
+        <CountryCard data={neighbourCountry} className="neighbour" />
+      )}
+    </div>
+  );
+}
+
+function CountryCard({ data, className = "" }) {
+  return (
+    <article className={`country ${className}`}>
+      <img className="country__img" src={data.flag} alt={data.name} />
       <div className="country__data">
         <h3 className="country__name">{data.name}</h3>
         <h4 className="country__region">{data.region}</h4>
+        <p className="country__row">
+          <span>👫</span> {(data.population / 1000000).toFixed(1)}M
+        </p>
 
         <p className="country__row">
-          <span>👫</span>
-          {data.population?.toLocaleString()}
+          <span>🗣️</span> {data.languages?.[0]?.name}
         </p>
         <p className="country__row">
-          <span>🗣️</span>
-          {data.languages?.[0]?.name}
-        </p>
-        <p className="country-row">
-          <span>💰</span>
-          {data.currencies?.[0]?.name}
+          <span>💰</span> {data.currencies?.[0]?.name}
         </p>
       </div>
-    </div>
+    </article>
   );
 }
